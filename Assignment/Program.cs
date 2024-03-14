0412b5cb0dfee4a11ac9117427c07e724209cbde
Vitaly Zinevich <vitaly.zinevich@outlook.com>
2024-03-14 10:34:20 +0200

    Cleanup

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -1,8 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-using Assignment.Logic;
+﻿using Assignment.Logic;
 
 Console.WriteLine("Please provide any integer number:");
-
 var inputValue = Console.ReadLine();
 
 var isInteger = int.TryParse(inputValue, out var sequenceNumber);
@@ -19,18 +17,13 @@ if (isInteger && sequenceNumber > 0)
     while (fibonacciTasks.Count > 0)
     {
         var finishedTask = await Task.WhenAny(fibonacciTasks);
-        if (finishedTask == fibonacciNumerOneTask)
-        {
-            Console.WriteLine($"Method call number one completed.");
-        }
-        else if (finishedTask == fibonacciNumerTwoTask)
-        {
-            Console.WriteLine("Method call number two completed.");
-        }
+
+        var whichOneIsCompleted = finishedTask == fibonacciNumerOneTask ? "First" : "Second";
+        Console.WriteLine($"{whichOneIsCompleted} call completed.");
+
         number = await finishedTask;
         fibonacciTasks.Remove(finishedTask);
     }
-
     Console.WriteLine($"The Fibonacci number at position {sequenceNumber} is {number}");
 }
 else
