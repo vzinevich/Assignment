84b7867b51430471d8fce38c659f2cad609eff4b
Vitaly Zinevich <vitaly.zinevich@outlook.com>
2024-03-14 10:52:34 +0200

    Doc added

--- Assignment/Logic/Fibonaccier.cs
+++ Assignment/Logic/Fibonaccier.cs
@@ -9,6 +9,12 @@ namespace Assignment.Logic
 {
     public class Fibonaccier : IFibonaccier
     {
+        /// <summary>
+        /// This method will return member of the Fibonacci sequence based on <paramref name="fibonacciNumber"/>
+        /// </summary>
+        /// <exception cref="ArgumentOutOfRangeException"></exception>
+        /// <param name="fibonacciNumber"></param>
+        /// <returns>Integer member of the Fibonacci row</returns>
         public async Task<int> GetFibonacciNumberAsync(int fibonacciNumber)
         {
             ArgumentOutOfRangeException.ThrowIfNegative(fibonacciNumber);
